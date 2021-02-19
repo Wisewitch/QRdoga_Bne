be79346b34088fea8ff568a93a222a1d5abb15fd
Wisewitch <agnes.gazdik@gmail.com>
2021-02-20 00:22:06 +0100

    frissités

--- app/src/main/java/com/e/qrdolgozat_babusane/MainActivity.java
+++ app/src/main/java/com/e/qrdolgozat_babusane/MainActivity.java
@@ -48,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
                 intentIntegrator.setBarcodeImageEnabled(true);
                 intentIntegrator.initiateScan();
             }
+
         });
     }
 
