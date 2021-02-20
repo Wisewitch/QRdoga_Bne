8a6592e78fef7135a7caa280d2b62ec3e64a1eef
Wisewitch <agnes.gazdik@gmail.com>
2021-02-20 20:04:48 +0100

    filebaírás próbálkozás...

--- app/src/main/java/com/e/qrdolgozat_babusane/MainActivity.java
+++ app/src/main/java/com/e/qrdolgozat_babusane/MainActivity.java
@@ -30,16 +30,14 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView textResult;
     private Button btnScan, btnKiir;
-    private boolean writePermissionGranted;
 
+    private boolean writePermissionGranted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
-
-
 
         btnScan.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,66 +51,91 @@ public class MainActivity extends AppCompatActivity {
                 intentIntegrator.initiateScan();
             }
 
-        });
+            });
+
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            textResult.setText("Nincs jogosultsága a filbeíráshoz");
+            String[] permissions =
+                    {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            ActivityCompat.requestPermissions(this, permissions, 0);
+
+            return;
+        }
+        textResult.setText("Engedély megadva");
+
+
     }
 
         @Override
-                protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-            IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-            if (result != null) {
-                Toast.makeText(this, "Kiléptél a scanből", Toast.LENGTH_SHORT).show();
-            } else {
+        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            if (requestCode == 0) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                        == PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                                == PackageManager.PERMISSION_GRANTED) {
 
-                textResult.setText("QRCode eredmény: " + result.getContents());
-            }
-
-            try {
-                Uri url = Uri.parse(result.getContents());
-                Intent intent = new Intent(Intent.ACTION_VIEW, url);
-                startActivity(intent);
-            } catch (Exception exception) {
-                Log.d("URI ERROR", exception.toString());
-            }
-
-            super.onActivityResult(requestCode, resultCode, data);
-
-
-            btnKiir.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-
-                    if (writePermissionGranted) {
-                        try {
-                            textResult.setText("QRCode eredmény: " + result.getContents());
-                            Naplozas.kiir(textResult.toString());
-                        } catch (IOException e) {
-                            Log.d("kiirasi hiba", e.getMessage());
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            });
-        }
-
-        // nem ír fileba mert ezzel van valami bibi...
-            /*@Override
-            public void onRequestPermissionResult ( int requestCode, @NonNull String[] permissions,
-            @NonNull int[] grantResults){
-                // super.onRequestPermissionResult(requestCode, requestCode, grantResults);
-                if (requestCode == 1) {
-                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-                            != PackageManager.PERMISSION_GRANTED &&
-                            ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                                    != PackageManager.PERMISSION_GRANTED) {
-                        writePermissionGranted = true;       }
+                    writePermissionGranted = true;
                 } else {
                     writePermissionGranted = false;
                 }
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-            }*/
+            }
+                }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            Toast.makeText(this, "Kiléptél a scanből", Toast.LENGTH_SHORT).show();
+        } else {
+
+            textResult.setText("QRCode eredmény: " + result.getContents());
+
+            if (writePermissionGranted) {
+                try {
+                    Naplozas.kiir(textResult.toString());
+                } catch (IOException e) {
+                    Log.d("kiirasi hiba", e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        try {
+            Uri url = Uri.parse(result.getContents());
+            Intent intent = new Intent(Intent.ACTION_VIEW, url);
+            startActivity(intent);
+        } catch (Exception exception) {
+            Log.d("URI ERROR", exception.toString());
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
 
 
+
+
+       btnKiir.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+             IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+            textResult.setText("QRCode eredmény: " + result.getContents());
+
+            if (writePermissionGranted) {
+                try {
+                    Naplozas.kiir(textResult.toString());
+                } catch (IOException e) {
+                    Log.d("kiirasi hiba", e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        }
+    });
+    }
 
     private void init() {
         btnKiir = findViewById(R.id.Button_kiir);
@@ -120,16 +143,16 @@ public class MainActivity extends AppCompatActivity {
         textResult = findViewById(R.id.textViewEredmeny);
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED &&
+                == PackageManager.PERMISSION_GRANTED &&
                 ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                        != PackageManager.PERMISSION_GRANTED) {
+                        == PackageManager.PERMISSION_GRANTED) {
 
             writePermissionGranted = true;
         } else {
             writePermissionGranted = false;
             String[] permissions =
                     {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
-            ActivityCompat.requestPermissions(this, permissions, 1);
+            ActivityCompat.requestPermissions(this, permissions, 0);
         }
     }
 
