84f8c517fbd59dbab63e871f9db99eec887473b5
Wisewitch <agnes.gazdik@gmail.com>
2021-02-20 01:15:09 +0100

    permission még nem teljesen működik... kikommenteltem...

--- app/src/main/java/com/e/qrdolgozat_babusane/MainActivity.java
+++ app/src/main/java/com/e/qrdolgozat_babusane/MainActivity.java
@@ -58,45 +58,57 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
                 protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-            IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+            IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
             if (result != null) {
                 Toast.makeText(this, "Kiléptél a scanből", Toast.LENGTH_SHORT).show();
-            }
-            else {
+            } else {
 
-                textResult.setText("QRCode eredmény: "+ result.getContents());
+                textResult.setText("QRCode eredmény: " + result.getContents());
             }
 
             try {
                 Uri url = Uri.parse(result.getContents());
                 Intent intent = new Intent(Intent.ACTION_VIEW, url);
                 startActivity(intent);
-            } catch (Exception exception){
+            } catch (Exception exception) {
                 Log.d("URI ERROR", exception.toString());
             }
 
-                    super.onActivityResult(requestCode, resultCode, data);
+            super.onActivityResult(requestCode, resultCode, data);
 
 
-           btnKiir.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+            btnKiir.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
 
-            if (writePermissionGranted) {
-                try {
-                    textResult.setText("QRCode eredmény: " + result.getContents());
-                    Naplozas.kiir(textResult.toString());
-                } catch (IOException e) {
-                    Log.d("kiirasi hiba", e.getMessage());
-                    e.printStackTrace();
+                    if (writePermissionGranted) {
+                        try {
+                            textResult.setText("QRCode eredmény: " + result.getContents());
+                            Naplozas.kiir(textResult.toString());
+                        } catch (IOException e) {
+                            Log.d("kiirasi hiba", e.getMessage());
+                            e.printStackTrace();
+                        }
+                    }
                 }
-            }
+            });
         }
-    });
-
-
-        }
+            /*@Override
+            public void onRequestPermissionResult ( int requestCode, @NonNull String[] permissions,
+            @NonNull int[] grantResults){
+                // super.onRequestPermissionResult(requestCode, requestCode, grantResults);
+                if (requestCode == 1) {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                            != PackageManager.PERMISSION_GRANTED &&
+                            ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                                    != PackageManager.PERMISSION_GRANTED) {
+                        writePermissionGranted = true;       }
+                } else {
+                    writePermissionGranted = false;
+                }
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            }*/
 
 
 
@@ -119,21 +131,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-        @Override
-        public void onRequestPermissionResult ( int requestCode, @NonNull String[] permissions,
-        @NonNull int[] grantResults){
-            // super.onRequestPermissionResult(requestCode, requestCode, grantResults);
-            if (requestCode == 1) {
-                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-                        != PackageManager.PERMISSION_GRANTED &&
-                        ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                                != PackageManager.PERMISSION_GRANTED) {
-                                     writePermissionGranted = true;       }
-            } else {
-                writePermissionGranted = false;
-            }
-             super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
+
+
     }
 
 
